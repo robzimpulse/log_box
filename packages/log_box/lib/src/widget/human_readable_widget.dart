663a0c2f4200aa12a7c553a43ab544eaf5c8a5d3
Robyarta Haruli Ruci <robyarta@bukalapak.com>
2025-12-19 08:16:46 +0700

    hide widget if null

--- packages/log_box/lib/src/widget/human_readable_widget.dart
+++ packages/log_box/lib/src/widget/human_readable_widget.dart
@@ -18,40 +18,66 @@ class HumanReadableWidget extends StatelessWidget {
   final Uint8List? image;
   final String? searchTerm;
 
+  Widget _header(BuildContext context) {
+    final value = this.value;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Flexible(
+          child: Text(
+            name,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        if (value != null && image == null)
+          IconButton(
+            icon: const Icon(Icons.copy, size: 16, color: Colors.grey),
+            onPressed: () => value.copyToClipboard(context: context),
+          )
+        else
+          SizedBox.shrink(),
+      ],
+    );
+  }
+
+  Widget _content(BuildContext context) {
+    final value = this.value;
+    final image = this.image;
+
+    if (image != null) {
+      return Image.memory(image);
+    }
+
+    if (value != null) {
+      return HighlightedTextWidget(
+        text: value.isJson ? value.prettify : value,
+        style: Theme.of(context).textTheme.labelSmall,
+        searchTerm: searchTerm,
+      );
+    }
+
+    return SizedBox.shrink();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final shouldShrink = [
+      value == null || value?.isEmpty == true,
+      image == null || image?.isEmpty == true,
+    ].every((e) => e);
+
+
+    if (shouldShrink) return SizedBox.shrink();
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const SizedBox(height: 8),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Flexible(
-              child: Text(
-                name,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Builder(
-              builder: (context) {
-                final value = this.value;
-
-                if (value != null && image == null) {
-                  return IconButton(
-                    icon: const Icon(Icons.copy, size: 16, color: Colors.grey),
-                    onPressed: () => value.copyToClipboard(context: context),
-                  );
-                }
-
-                return SizedBox();
-              },
-            ),
-          ],
-        ),
+        _header(context),
         SizedBox(
           width: double.infinity,
           child: Card(
@@ -61,29 +87,10 @@ class HumanReadableWidget extends StatelessWidget {
             ),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Builder(
-                builder: (context) {
-                  final image = this.image;
-                  if (image != null) {
-                    return Image.memory(image);
-                  }
-
-                  final value = this.value;
-
-                  if (value != null) {
-                    return HighlightedTextWidget(
-                      text: value.isJson ? value.prettify : value,
-                      style: Theme.of(context).textTheme.labelSmall,
-                      searchTerm: searchTerm,
-                    );
-                  }
-
-                  return SizedBox();
-                },
-              ),
+              child: _content(context),
             ),
           ),
-        ),
+        )
       ],
     );
   }
