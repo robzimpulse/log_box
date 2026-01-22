5a32ca1e19791b364fd5591c59150805e091b1df
Robyarta <robyarta.haruli@allofresh.id>
2026-01-22 09:49:52 +0700

    adjust

--- packages/log_box_persistent_storage_drift/lib/src/adapter/executor_adapter/executor_adapter_io.dart
+++ packages/log_box_persistent_storage_drift/lib/src/adapter/executor_adapter/executor_adapter_io.dart
@@ -4,7 +4,7 @@ import '../../database/executor/base.dart';
 import '../../database/executor/file_executor.dart';
 
 Future<Executor> getExecutor() async {
-  return getApplicationSupportDirectory().then(
+  return getApplicationDocumentsDirectory().then(
     (e) => FileExecutor(path: e.path),
   );
 }
