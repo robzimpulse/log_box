ec5fc783daaf99bfe1c22859130384d058d3ad71
Robyarta <robyarta.haruli@allofresh.id>
2026-01-23 11:15:19 +0700

    adjust executor io for unit test

--- packages/log_box_persistent_storage_drift/lib/memory_executor.dart
+++ packages/log_box_persistent_storage_drift/lib/memory_executor.dart
@@ -6,13 +6,11 @@ import 'src/database/executor/base.dart';
 class MemoryExecutor extends Executor {
   final QueryExecutor _executor;
 
-  MemoryExecutor({QueryExecutor? executor})
-    : _executor =
-          executor ??
-          DatabaseConnection(
-            NativeDatabase.memory(),
-            closeStreamsSynchronously: true,
-          );
+  MemoryExecutor()
+    : _executor = DatabaseConnection(
+        NativeDatabase.memory(),
+        closeStreamsSynchronously: true,
+      );
 
   @override
   QueryExecutor get executor => _executor;
--- packages/log_box_persistent_storage_drift/lib/src/adapter/executor_adapter/executor_adapter_io.dart
+++ packages/log_box_persistent_storage_drift/lib/src/adapter/executor_adapter/executor_adapter_io.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
+import 'package:log_box_persistent_storage_drift/memory_executor.dart';
 import 'package:path_provider/path_provider.dart';
 
 import '../../database/executor/base.dart';
 import '../../database/executor/file_executor.dart';
 
 Future<Executor> getExecutor() async {
+  if (Platform.environment.containsKey('FLUTTER_TEST')) {
+    return MemoryExecutor();
+  }
   return getApplicationDocumentsDirectory().then(
     (e) => FileExecutor(path: e.path),
   );
