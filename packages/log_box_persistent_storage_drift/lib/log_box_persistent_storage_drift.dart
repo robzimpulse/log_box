13daef3768927d3812c25f0f53ce80e763e87e4e
Robyarta <robyarta.haruli@allofresh.id>
2026-01-28 16:57:13 +0700

    forgot expose extension

--- packages/log_box_persistent_storage_drift/lib/log_box_persistent_storage_drift.dart
+++ packages/log_box_persistent_storage_drift/lib/log_box_persistent_storage_drift.dart
@@ -2,3 +2,4 @@ export 'src/drift_persistent_storage.dart';
 export 'src/database/executor/base.dart';
 export 'src/util/typedef.dart';
 export 'src/interceptor/drift_query_interceptor.dart';
+export 'src/extension/logbox_extension.dart';
