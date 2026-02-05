ff40355fc5833fb5242a3bb8968e43a1111e030b
Robyarta <robyarta.haruli@allofresh.id>
2026-02-05 22:38:13 +0700

    adjust grouper

--- packages/log_box_persistent_storage_drift/lib/src/extension/logbox_extension.dart
+++ packages/log_box_persistent_storage_drift/lib/src/extension/logbox_extension.dart
@@ -1,13 +1,14 @@
 import 'package:log_box/log_box.dart';
+import 'package:log_box_persistent_storage_drift/src/model/drift_query_entry_model.dart';
 
 import '../interceptor/drift_query_interceptor.dart';
-import '../util/drift_log_builder.dart';
 import '../util/drift_log_grouper.dart';
 
 extension DriftInterceptorExtension on LogBox {
   DriftQueryInterceptor get queryInterceptor {
-    final builder = DriftLogBuilder(onEntry: (e) => storage.add(log: e));
-    final grouper = DriftLogGrouper(onGroup: builder.add);
+    final grouper = DriftLogGrouper(
+      onGroup: (e) => storage.add(log: DriftQueryEntryModel(operations: e)),
+    );
     return DriftQueryInterceptor(onEvent: grouper.add);
   }
 }
